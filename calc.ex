f3411c3ee3caa11b3c49663a695d189d8316065f
tophat8855 <hrfarley@gmail.com>
2017-07-15 13:38:50 -0700

    add check for zero? in calculator

--- calc.ex
+++ calc.ex
@@ -10,7 +10,7 @@ defmodule Calc do
     Calc.add(1, 2)  #3
     Calc.add(4)     #4
   """
-  def add(a, b \\ 1) do
+  def add(a, b \\ 1) when is_integer(a) and is_integer(b) do
     a + b
   end
 
@@ -21,6 +21,13 @@ defmodule Calc do
   def double(a) do
     a + a
   end
+
+  def divide(b, a) do
+    b / a
+  end
+
+  def zero?(0), do: true
+  def zero?(_), do: false
 
   defmacro __using__(_) do
     quote do
